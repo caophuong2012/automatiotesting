5bb4b8ea92cd1030278453835f916673e5c1b228
Phuong Cao <caophuong2012@gmail.com>
2020-01-08 16:17:43 +0700

    Add test.py
    Signed-off-by: Phuong Cao <caophuong2012@gmail.com>

---
+++ test.py
@@ -0,0 +1,10 @@
+import time
+from selenium import webdriver
+driver = webdriver.Chrome('C:/Users/phuongcao12691/Downloads/chromedriver.exe')  # Optional argument, if not specified will search path.
+driver.get('https://beowulfchain.com/');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(1) # Let the user actually see something!
+driver.quit()
